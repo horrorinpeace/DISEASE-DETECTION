b5712984514e15fdfd2ce6d2a9e3b11ac15de4f7
horrorinpeace <horrorinpeace@gmail.com>
2025-12-04 21:17:14 +0530

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -126,7 +126,17 @@ try:
 except:
     st.error("Model failed to load.")
     model=None
-    CLASS_NAMES=[]
+    CLASS_NAMES= [
+        'CURRY POWDERY MILDEW', 'HEALTHY MILLET', 'HEALTHY POTATO', 'HEALTHY RICE',
+        'HEALTHY SUGARCANE', 'HEALTHY TEA LEAF', 'HEALTHY TOMATO', 'HEALTHY WHEAT',
+        'MILLETS BLAST', 'MILLETS RUST', 'POTATO EARLY BLIGHT', 'POTATO LATE BLIGHT',
+        'RICE BACTERIAL BLIGHT', 'RICE BROWN SPOT', 'RICE LEAF SMUT', 'SUGARCANE RED ROT',
+        'SUGARCANE RUST', 'SUGARCANE YELLOW', 'TEA GRAY BLIGHT', 'TEA GREEN MIRID BUG',
+        'TEA HELOPELTIS', 'TOMATO LEAF MOLD', 'TOMATO MOSAIC VIRUS',
+        'TOMATO SEPTORIA LEAF SPOT', 'WHEAT BROWN RUST', 'WHEAT LOOSE SMUT',
+        'WHEAT YELLOW RUST'
+    ]
+
 
 if "report_text" not in st.session_state:st.session_state.report_text=""
 if "selected_language"not in st.session_state:st.session_state.selected_language="English"
@@ -382,3 +392,4 @@ if st.session_state.report_text:
 # ==========================
 st.markdown("---")
 st.markdown("<div class='caption'>FarmDoc ©2025 — Helping Farmers Grow Smarter</div>",unsafe_allow_html=True)
+
